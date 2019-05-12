6a61abab5f3a6670ceccb8303fcf3b2e2433b7c8
Dhanilan <mail2dhanilan@gmail.com>
2019-05-12 18:29:42 +0530

    SVM

---
+++ Supervised Learning/SVM/svm.R
@@ -0,0 +1,26 @@
+data(iris)
+attach(iris)
+library(e1071 )
+
+
+## classification mode
+# default with factor response:
+
+
+model <- svm(Species ~ ., data = iris)
+
+# alternatively the traditional interface:
+x <- subset(iris, select = -Species)
+y <- Species
+model <- svm(x, y) 
+
+print(model)
+summary(model)
+
+# test with train data
+pred <- predict(model, x)
+# (same as:)
+pred <- fitted(model)
+
+# Check accuracy:
+table(pred, y)
