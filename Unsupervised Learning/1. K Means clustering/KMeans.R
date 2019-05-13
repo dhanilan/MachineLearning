bb4d4bd654cba8d9a291f13546240db26dc8d9ff
Dhanilan <mail2dhanilan@gmail.com>
2019-05-13 07:44:37 +0530

    KMeans
    KMeans

---
+++ Unsupervised Learning/1. K Means clustering/KMeans.R
@@ -0,0 +1,12 @@
+#kmeans(x, centers, iter.max = 10, nstart = 1, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"), trace=FALSE)
+
+require(graphics)
+
+# a 2-dimensional example
+x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 2),
+           matrix(rnorm(100, mean = 1, sd = 0.3), ncol = 2))
+colnames(x) <- c("x", "y")
+(cl <- kmeans(x, 2))
+#(cl <- kmeans(x, 5, nstart = 25))
+plot(x, col = cl$cluster)
+points(cl$centers, col = 1:2, pch = 8, cex = 2)
